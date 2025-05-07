3cced6fa40ff813f12a5bc720406dab8cb946353
P H Aju <aju.ph@exorint.com>
2025-05-07 19:05:16 +0530

    feat(axum): testing static web server

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,45 @@
-mod controller;
+// use std::{thread, time::Duration};
 
-use std::{thread, time::Duration};
+// mod controller;
+// use controller::SerialController;
 
-use controller::SerialController;
+use axum::{
+    body::Body,
+    http::{Response, StatusCode},
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use std::path::PathBuf;
+use tower_http::services::ServeDir;
+use tokio::fs;
+const ADDR: &str = "127.0.0.1:3000";
 
-fn main() {
-    let mut port = SerialController::open_port().unwrap();
-    thread::sleep(Duration::from_secs(12));
-    let response = port.write(101).unwrap();
-    println!("{}", response);
+#[tokio::main]
+async fn main() {
+
+    // let mut port = SerialController::open_port().unwrap();
+    // thread::sleep(Duration::from_secs(12));
+    // let response = port.write(101).unwrap();
+    // println!("{}", response);
+
+    let app = Router::new()
+        .route("/", get(serve_index))
+        .fallback_service(ServeDir::new("assets"));
+
+    let listener = tokio::net::TcpListener::bind(ADDR).await.unwrap();
+    println!("Server running on: {}", ADDR);
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn serve_index() -> impl IntoResponse {
+    let path = PathBuf::from("assets/index.html");
+
+    match fs::read(&path).await {
+        Ok(contents) => Response::builder()
+            .header("Content-Type", "text/html")
+            .body(Body::from(contents))
+            .unwrap(),
+        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Failed to load index.html").into_response(),
+    }
 }
