194658a96c90d93f1f4933fcd68aa601f88a3350
SereneVoid <ajuph9224@gmail.com>
2025-05-08 09:42:14 +0530

    refactor(axum): hosting sample page succeeded

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,21 @@ use axum::{
     body::Body,
     http::{Response, StatusCode},
     response::IntoResponse,
-    routing::get,
+    routing::{get, post},
     Router,
+    Json
 };
-use std::path::PathBuf;
+use std::{
+    path::{Path, PathBuf},
+    sync::{Arc, Mutex}
+};
 use tower_http::services::ServeDir;
 use tokio::fs;
-const ADDR: &str = "127.0.0.1:3000";
+use serde_json::{json, Value};
+
+type SharedJson = Arc<Mutex<Value>>;
+
+const ADDR: &str = "0.0.0.0:3402";
 
 #[tokio::main]
 async fn main() {
@@ -23,17 +31,30 @@ async fn main() {
     // let response = port.write(101).unwrap();
     // println!("{}", response);
 
+    let data = load_or_init_json("data.json");
+    let state = Arc::new(Mutex::new(data));
+
     let app = Router::new()
         .route("/", get(serve_index))
-        .fallback_service(ServeDir::new("assets"));
+        .fallback_service(ServeDir::new("public"))
+        .route("/data.json", get(serve_data));
 
     let listener = tokio::net::TcpListener::bind(ADDR).await.unwrap();
     println!("Server running on: {}", ADDR);
     axum::serve(listener, app).await.unwrap();
 }
 
+fn load_or_init_json(path: &str) -> Value {
+    if Path::new(path).exists() {
+        let contents = fs::read_to_string(path).unwrap_or_else(|_| "{}".into());
+        serde_json::from_str(&contents).unwrap_or_else(|_| json!({}))
+    } else {
+        json!({})
+    }
+}
+
 async fn serve_index() -> impl IntoResponse {
-    let path = PathBuf::from("assets/index.html");
+    let path = PathBuf::from("public/index.html");
 
     match fs::read(&path).await {
         Ok(contents) => Response::builder()
@@ -42,4 +63,8 @@ async fn serve_index() -> impl IntoResponse {
             .unwrap(),
         Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Failed to load index.html").into_response(),
     }
+}
+
+async fn serve_data() -> impl IntoResponse {
+    
 }
