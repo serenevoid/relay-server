47dfa3ce651d3e68981ef6add2e7a6c6fa332c75
SereneVoid <ajuph9224@gmail.com>
2025-05-09 19:51:40 +0530

    refactor(axum): communication with serial port on post request

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,86 @@
-// use std::{thread, time::Duration};
-
-// mod controller;
-// use controller::SerialController;
+mod controller;
+use controller::SerialController;
 
 use axum::{
     body::Body,
     http::{Response, StatusCode},
     response::IntoResponse,
-    routing::{get, post},
-    Router,
-    Json
+    routing::get,
+    extract::{State, Json},
+    Router
 };
 use std::{
     path::{Path, PathBuf},
-    sync::{Arc, Mutex}
+    sync::{Arc, Mutex},
+    fs
 };
 use tower_http::services::ServeDir;
-use tokio::fs;
-use serde_json::{json, Value};
+use serde::{Serialize, Deserialize};
 
-type SharedJson = Arc<Mutex<Value>>;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Item {
+    id: u32,
+    name: String,
+    panel_category: String,
+    ipv4: String,
+    state: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct States {
+    items: Vec<Item>,
+}
+
+type SharedJson = Arc<Mutex<States>>;
 
 const ADDR: &str = "0.0.0.0:3402";
+const CONFIG_PATH: &str = "./data.json";
 
 #[tokio::main]
 async fn main() {
-
-    // let mut port = SerialController::open_port().unwrap();
-    // thread::sleep(Duration::from_secs(12));
-    // let response = port.write(101).unwrap();
-    // println!("{}", response);
-
-    let data = load_or_init_json("data.json");
+    let data = load_or_init_json(CONFIG_PATH);
     let state = Arc::new(Mutex::new(data));
+
+    SerialController::open_port();
 
     let app = Router::new()
         .route("/", get(serve_index))
         .fallback_service(ServeDir::new("public"))
-        .route("/data.json", get(serve_data));
+        .route("/data.json", get(serve_data_handler).post(receive_data_handler))
+        .with_state(state.clone());
 
     let listener = tokio::net::TcpListener::bind(ADDR).await.unwrap();
-    println!("Server running on: {}", ADDR);
+    println!("Server running on: {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
-fn load_or_init_json(path: &str) -> Value {
+fn load_or_init_json(path: &str) -> States {
     if Path::new(path).exists() {
         let contents = fs::read_to_string(path).unwrap_or_else(|_| "{}".into());
-        serde_json::from_str(&contents).unwrap_or_else(|_| json!({}))
+        serde_json::from_str(&contents).unwrap_or_else(|_| States { items: vec![] })
     } else {
-        json!({})
+        States { items: (1..=10)
+            .map(|i| Item {
+                id: i,
+                name: String::from("user"),
+                panel_category: String::from("panel_category"),
+                ipv4: String::from("-.-.-.-"),
+                state: false
+            })
+            .collect()
+        }
     }
+}
+
+fn save_json(data: &States) -> Result<(), std::io::Error> {
+    fs::write(CONFIG_PATH, serde_json::to_string_pretty(data).unwrap())
 }
 
 async fn serve_index() -> impl IntoResponse {
     let path = PathBuf::from("public/index.html");
 
-    match fs::read(&path).await {
+    match fs::read(&path) {
         Ok(contents) => Response::builder()
             .header("Content-Type", "text/html")
             .body(Body::from(contents))
@@ -65,6 +89,25 @@ async fn serve_index() -> impl IntoResponse {
     }
 }
 
-async fn serve_data() -> impl IntoResponse {
-    
+async fn serve_data_handler(State(state): State<SharedJson>) -> impl IntoResponse {
+    let data = state.lock().unwrap();
+    axum::Json(data.clone())
+}
+
+async fn receive_data_handler(State(state): State<SharedJson>, Json(updated_item): Json<Item>) -> impl IntoResponse {
+    let mut data = state.lock().unwrap();
+    if let Some(existing_item) = data.items.iter_mut().find(|item| item.id == updated_item.id) {
+        existing_item.name = updated_item.name;
+        existing_item.ipv4 = updated_item.ipv4;
+        existing_item.panel_category = updated_item.panel_category;
+        existing_item.state = updated_item.state;
+        SerialController::write("101");
+        if let Err(e) = save_json(&data) {
+            eprintln!("Error saving data to file: {}", e);
+            return StatusCode::INTERNAL_SERVER_ERROR;
+        }
+        StatusCode::OK
+    } else {
+        StatusCode::NOT_FOUND
+    }
 }
