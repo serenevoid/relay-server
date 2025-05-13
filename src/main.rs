8da2264ae4a4ea3eb1c1affdfe195d82f925e222
SereneVoid <ajuph9224@gmail.com>
2025-05-13 21:47:56 +0530

    fix(web): finished frontend and modifying backend

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
 mod controller;
 
 use axum::{
-    body::Body,
-    http::{Response, StatusCode},
-    response::IntoResponse,
-    routing::get,
-    extract::{State, Json},
-    Router
+    body::Body, extract::{Json, Query, State}, http::{Response, StatusCode}, response::IntoResponse, routing::get, Router
 };
+use tokio::sync::{broadcast, Mutex};
 use std::{
-    path::{Path, PathBuf},
-    sync::{Arc, Mutex},
-    fs
+    fs, path::{Path, PathBuf}, sync::Arc, time::Duration
 };
 use tower_http::services::ServeDir;
 use serde::{Serialize, Deserialize};
 
+#[derive(Debug, Deserialize)]
+pub struct InitialQueryParams {
+    pub initial_event: Option<String>,
+}
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 struct Item {
@@ -27,11 +25,19 @@ struct Item {
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-struct States {
+pub struct States {
     relays: Vec<Item>,
 }
 
-type SharedJson = Arc<Mutex<States>>;
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ServerEvent {
+    updated_item: Item,
+}
+
+pub struct AppState {
+    pub json_data: Arc<Mutex<States>>,
+    pub tx: broadcast::Sender<ServerEvent>
+}
 
 const ADDR: &str = "0.0.0.0:3402";
 const CONFIG_PATH: &str = "./data.json";
@@ -40,12 +46,17 @@ const CONFIG_PATH: &str = "./data.json";
 async fn main() {
     let data = load_or_init_json(CONFIG_PATH);
     let state = Arc::new(Mutex::new(data));
+    let (tx, _rx) = broadcast::channel::<ServerEvent>(16);
+    let app_state = Arc::new(AppState {
+        json_data: state.clone(),
+        tx
+    });
 
     let app = Router::new()
         .route("/", get(serve_index))
         .fallback_service(ServeDir::new("public"))
-        .route("/data.json", get(serve_data_handler).post(receive_data_handler))
-        .with_state(state.clone());
+        .route("/data", get(serve_data_handler).post(receive_data_handler))
+        .with_state(app_state.clone());
 
     let listener = tokio::net::TcpListener::bind(ADDR).await.unwrap();
     println!("Server running on: {}", listener.local_addr().unwrap());
@@ -86,27 +97,78 @@ async fn serve_index() -> impl IntoResponse {
     }
 }
 
-async fn serve_data_handler(State(state): State<SharedJson>) -> impl IntoResponse {
-    let data = state.lock().unwrap();
-    Json(data.clone())
+async fn serve_data_handler(
+    State(state): State<Arc<AppState>>,
+    Query(query_params): Query<InitialQueryParams>
+    ) -> impl IntoResponse {
+    if query_params.initial_event.is_some() {
+        let data = state.json_data.lock().await;
+        return (StatusCode::OK, Json(data.clone())).into_response();
+    }
+    let mut rx = state.tx.subscribe();
+    let poll_timeout = Duration::from_secs(60);
+
+    let event_future = async {
+        loop {
+            match rx.recv().await {
+                Ok(event) => {
+                    return Ok(event);
+                }
+                Err(broadcast::error::RecvError::Lagged(_count)) => {
+                    return Err(StatusCode::RESET_CONTENT);
+                }
+                Err(broadcast::error::RecvError::Closed) => {
+                    return Err(StatusCode::INTERNAL_SERVER_ERROR);
+                }
+            }
+        }
+    };
+
+    match tokio::time::timeout(poll_timeout, event_future).await {
+        Ok(Ok(event)) => {
+            (StatusCode::OK, Json(event)).into_response()
+        }
+        Ok(Err(status)) => status.into_response(),
+        Err(_) => {
+            StatusCode::NO_CONTENT.into_response()
+        }
+    }
 }
 
-async fn receive_data_handler(State(state): State<SharedJson>, Json(updated_item): Json<Item>) -> impl IntoResponse {
-    let mut data = state.lock().unwrap();
-    let status_code: Option<StatusCode>;
-    if let Some(existing_item) = data.relays.iter_mut().find(|item| item.id == updated_item.id) {
-        existing_item.name = updated_item.name;
-        existing_item.ipv4 = updated_item.ipv4;
-        existing_item.panel_category = updated_item.panel_category;
-        existing_item.state = updated_item.state;
+async fn receive_data_handler(
+    State(state): State<Arc<AppState>>,
+    Json(updated_item): Json<Item>
+    ) -> impl IntoResponse {
+    let mut data = state.json_data.lock().await;
+    let mut item_updated = false;
+    let mut status_code = Some(StatusCode::NOT_FOUND);
+    let mut updated_item_for_event: Option<Item> = None;
+    let mut relay_state: u16 = 0;
+    for item in &mut data.relays {
+        if item.id == updated_item.id {
+            item.name = updated_item.name.clone();
+            item.ipv4 = updated_item.ipv4.clone();
+            item.panel_category = updated_item.panel_category.clone();
+            item.state = updated_item.state;
+            updated_item_for_event = Some(updated_item.clone());
+            item_updated = true;
+        }
+        if item.state {
+            relay_state |= 1 << (item.id - 1);
+        }
+    };
+    if item_updated {
         if let Err(e) = save_json(&data) {
             eprintln!("Error saving data to file: {}", e);
-            return StatusCode::INTERNAL_SERVER_ERROR;
+            status_code = Some(StatusCode::INTERNAL_SERVER_ERROR);
+        } else {
+            if let Some(updated) = updated_item_for_event {
+                let event = ServerEvent { updated_item: updated };
+                if let Err(_e) = state.tx.send(event) {}
+            }
+            status_code = Some(StatusCode::OK);
         }
-        status_code = Some(StatusCode::OK);
-    } else {
-        status_code = Some(StatusCode::NOT_FOUND);
     }
-    println!("{}", controller::set_relays(101).unwrap());
+    println!("{}", controller::set_relays(relay_state).unwrap());
     status_code.unwrap()
 }
